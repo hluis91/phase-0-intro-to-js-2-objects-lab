ba3353a06f4bfef36d437379a4020a03f8146d36
Luis Hernandez <lshrnndz6@gmail.com>
2022-12-22 12:57:37 -0800

    commit tree 225b9c3e0a8d74a446102990471a282f61228573

--- index.js
+++ index.js
@@ -1,1 +1,238 @@
 // Write your solution in this file!
+
+const employee = {
+    name : 'Jose',
+    streetAddress: '123 Fake St.'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {
+        ...employee,
+        [key]: value,
+      };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newKey = {...employee[key]}
+    delete newKey[key]
+    return newKey;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const employee = {
+//     name: "Jose",
+//     streetAddress: "123 Fake Street"
+// };
+
+// function updateEmployeeWithKeyAndValue(employee, key, value) {
+//     const newObj = {...employee};
+//     newObj[key] = value;
+//     return newObj
+// }
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+//     employee[key] = value;
+//     return employee
+// }
+
+// function deleteFromEmployeeByKey(employee, key) {
+//     const newKey = {...employee[key]}
+//     delete newKey[key]
+//     return newKey;
+// }
+
+// function destructivelyDeleteFromEmployeeByKey(employee, key) {
+//     delete employee[key]
+//     return employee
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
